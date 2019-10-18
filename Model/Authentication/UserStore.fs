ac2fa4797550f0984a9bdf3c3cd7b62a3d4e566d
Anatoly Safonov <hartigans@live.com>
2019-10-19 00:24:23 +0300

    Handle CancellationToken correctly

--- Model/Authentication/UserStore.fs
+++ Model/Authentication/UserStore.fs
@@ -16,43 +16,39 @@ type UserStore(context: UserContext, logger: ILogger<UserStore>) =
     interface IUserPasswordStore<UserIdentity> with
         member this.GetPasswordHashAsync(user, cancellationToken) =
             taskC cancellationToken {
+                cancellationToken.ThrowIfCancellationRequested()
                 return user.PasswordHash
             }
 
         member this.HasPasswordAsync(user, cancellationToken) = 
             taskC cancellationToken {
+                cancellationToken.ThrowIfCancellationRequested()
                 return true
             }
 
         member this.SetPasswordHashAsync(user, passwordHash, cancellationToken) = 
             taskUC cancellationToken {
+                cancellationToken.ThrowIfCancellationRequested()
                 user.PasswordHash <- passwordHash
             }
 
         member this.DeleteAsync(user, cancellationToken) = 
             taskC cancellationToken {
-                if not cancellationToken.IsCancellationRequested then
-                    let entity = user.ToEntity()
-                    let! result = (context :> IContext<User>).Remove(entity)
-                    match result with
-                    | Result.Ok(ok) ->
-                        logger.LogInformation(sprintf "User %s successefuly deleted" user.Name)
-                        return IdentityResult.Success
-                    | Result.Error(error) ->
-                        match error with
-                        | RemoveFail.Error(ex) ->
-                            logger.LogError(ex, sprintf "User %s not deleted" user.Name)
-                            let identityError = IdentityError()
-                            identityError.Code <- ex.HResult.ToString()
-                            identityError.Description <- ex.Message
-                            return IdentityResult.Failed(identityError)
-                else
-                    logger.LogInformation(sprintf "User %s not deleted because operation canceled" user.Name)
-                    let ex = OperationCanceledException(cancellationToken)
-                    let identityError = IdentityError()
-                    identityError.Code <- ex.HResult.ToString()
-                    identityError.Description <- ex.Message
-                    return IdentityResult.Failed(identityError)
+                cancellationToken.ThrowIfCancellationRequested()
+                let entity = user.ToEntity()
+                let! result = (context :> IContext<User>).Remove(entity)
+                match result with
+                | Result.Ok(ok) ->
+                    logger.LogInformation(sprintf "User %s successefuly deleted" user.Name)
+                    return IdentityResult.Success
+                | Result.Error(error) ->
+                    match error with
+                    | RemoveFail.Error(ex) ->
+                        logger.LogError(ex, sprintf "User %s not deleted" user.Name)
+                        let identityError = IdentityError()
+                        identityError.Code <- ex.HResult.ToString()
+                        identityError.Description <- ex.Message
+                        return IdentityResult.Failed(identityError)
             }
 
         member this.Dispose(): unit = 
@@ -60,6 +56,7 @@ type UserStore(context: UserContext, logger: ILogger<UserStore>) =
 
         member this.FindByIdAsync(userId, cancellationToken) = 
             taskC cancellationToken {
+                cancellationToken.ThrowIfCancellationRequested()
                 let entity = User()
                 entity.Id <- userId
                 let! result = (context :> IContext<User>).Get(entity)
@@ -78,6 +75,7 @@ type UserStore(context: UserContext, logger: ILogger<UserStore>) =
 
         member this.FindByNameAsync(normalizedUserName, cancellationToken) = 
             taskC cancellationToken {
+                cancellationToken.ThrowIfCancellationRequested()
                 let! result = context.GetByName(normalizedUserName)
                 match result with
                 | Result.Ok(user) ->
@@ -94,78 +92,69 @@ type UserStore(context: UserContext, logger: ILogger<UserStore>) =
 
         member this.GetNormalizedUserNameAsync(user, cancellationToken) = 
             taskC cancellationToken {
+                cancellationToken.ThrowIfCancellationRequested()
                 return user.NormalizedName
             }
 
         member this.GetUserIdAsync(user, cancellationToken) = 
             taskC cancellationToken {
+                cancellationToken.ThrowIfCancellationRequested()
                 return user.Id.ToString()
             }
 
         member this.GetUserNameAsync(user, cancellationToken) = 
             taskC cancellationToken {
+                cancellationToken.ThrowIfCancellationRequested()
                 return user.Name
             }
 
         member this.SetNormalizedUserNameAsync(user, normalizedName, cancellationToken) = 
             taskUC cancellationToken {
+                cancellationToken.ThrowIfCancellationRequested()
                 user.NormalizedName <- normalizedName
                 return user
             }
 
         member this.SetUserNameAsync(user, userName, cancellationToken) = 
             taskUC cancellationToken {
+                cancellationToken.ThrowIfCancellationRequested()
                 user.Name <- userName
                 return user
             }
 
         member this.UpdateAsync(user, cancellationToken) = 
             taskC cancellationToken {
-                if not cancellationToken.IsCancellationRequested then
-                    let entity = user.ToEntity()
-                    let! result = (context :> IContext<User>).Update(entity, fun _ -> entity)
-                    match result with
-                    | Result.Ok(ok) ->
-                        return IdentityResult.Success
-                    | Result.Error(fail) ->
-                        match fail with
-                            | UpdateFail.Error(ex) ->
-                                logger.LogError(sprintf "User %s not updated" user.Name, ex)
-                                let identityError = IdentityError()
-                                identityError.Code <- ex.HResult.ToString()
-                                identityError.Description <- ex.Message
-                                return IdentityResult.Failed(identityError)
-                else
-                    logger.LogError(sprintf "User %s not updated because operation canceled" user.Name)
-                    let ex = OperationCanceledException(cancellationToken)
-                    let identityError = IdentityError()
-                    identityError.Code <- ex.HResult.ToString()
-                    identityError.Description <- ex.Message
-                    return IdentityResult.Failed(identityError)
-            }
-
-        member this.CreateAsync(user, cancellationToken) =
-            taskC cancellationToken {
-                if not cancellationToken.IsCancellationRequested then
-                    let entity = user.ToEntity()
-                    let! result = (context :> IContext<User>).Insert(entity, entity)
-                    match result with
-                    | Result.Ok(ok) ->
-                        logger.LogInformation(sprintf "User %s successfuly added" user.Name)
-                        return IdentityResult.Success
-                    | Result.Error(error) ->
-                        match error with
-                        | InsertFail.Error(ex) ->
-                            logger.LogError(ex, sprintf "User %s not added" user.Name)
+                cancellationToken.ThrowIfCancellationRequested()
+                let entity = user.ToEntity()
+                let! result = (context :> IContext<User>).Update(entity, fun _ -> entity)
+                match result with
+                | Result.Ok(ok) ->
+                    return IdentityResult.Success
+                | Result.Error(fail) ->
+                    match fail with
+                        | UpdateFail.Error(ex) ->
+                            logger.LogError(sprintf "User %s not updated" user.Name, ex)
                             let identityError = IdentityError()
                             identityError.Code <- ex.HResult.ToString()
                             identityError.Description <- ex.Message
                             return IdentityResult.Failed(identityError)
-                else
-                    logger.LogError(sprintf "User %s not added because operation canceled" user.Name)
-                    let ex = OperationCanceledException(cancellationToken)
-                    let identityError = IdentityError()
-                    identityError.Code <- ex.HResult.ToString()
-                    identityError.Description <- ex.Message
-                    return IdentityResult.Failed(identityError)
+            }
+
+        member this.CreateAsync(user, cancellationToken) =
+            taskC cancellationToken {
+                cancellationToken.ThrowIfCancellationRequested()
+                let entity = user.ToEntity()
+                let! result = (context :> IContext<User>).Insert(entity, entity)
+                match result with
+                | Result.Ok(ok) ->
+                    logger.LogInformation(sprintf "User %s successfuly added" user.Name)
+                    return IdentityResult.Success
+                | Result.Error(error) ->
+                    match error with
+                    | InsertFail.Error(ex) ->
+                        logger.LogError(ex, sprintf "User %s not added" user.Name)
+                        let identityError = IdentityError()
+                        identityError.Code <- ex.HResult.ToString()
+                        identityError.Description <- ex.Message
+                        return IdentityResult.Failed(identityError)
             }
