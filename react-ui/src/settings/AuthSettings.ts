b15c195afb85573dd51c43d2bc37756ffb7e44cd
Anatoly Safonov <hartigans@live.com>
2020-07-02 01:36:55 +0300

    Rename client

--- react-ui/src/settings/AuthSettings.ts
+++ react-ui/src/settings/AuthSettings.ts
@@ -1,6 +1,6 @@
 export class AuthSettings {
     public static Authority = 'http://localhost';
-    public static ClientId = 'react-ui';
+    public static ClientId = 'template-service-ui-debug';
     public static ClientRoot = 'http://localhost:3000/';
     public static ClientScope = 'web_api openid profile role';
 }
