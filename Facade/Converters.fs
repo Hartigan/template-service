bfb7392c3a18377b07b8b5286dd2d27e0ef335fa
Anatoly Safonov <hartigans@live.com>
2021-01-18 23:48:46 +0300

    Implemented ExaminationApi

---
+++ Facade/Converters.fs
@@ -0,0 +1,169 @@
+namespace Facade
+
+open Domain
+open System
+
+type BackConverter private () =
+    static member Convert(o: DateInterval) : Contexts.SearchInterval<DateTimeOffset> =
+        {
+            From = DateTimeOffset(o.Start, TimeSpan())
+            To = DateTimeOffset(o.End, TimeSpan())
+        }
+
+    static member Convert(o: UInt32Interval) : Contexts.SearchInterval<uint32> =
+        {
+            From = o.Start
+            To = o.End
+        }
+
+    static member Convert(o: Int32Interval) : Contexts.SearchInterval<int32> =
+        {
+            From = o.Start
+            To = o.End
+        }
+
+type Converter private () =
+
+    static member Convert(o: DateTimeOffset) =
+        o.Ticks
+
+    static member Convert<'IN, 'OUT>(input: List<'IN>, output: Google.Protobuf.Collections.RepeatedField<'OUT>, converter: 'IN -> 'OUT) =
+        input |> List.iter(fun item -> output.Add(converter(item)))
+
+    static member Convert(o: Models.Reports.SubmissionPreviewModel) =
+        let result = SubmissionPreview()
+        result.Id <- o.Id.Value
+        result.StartedAt <- Converter.Convert(o.StartedAt)
+        result.Deadline <- Converter.Convert(o.Deadline)
+        result.Title <- o.Title.Value
+        result.Completed <- o.Completed
+        result.Author <- Converter.Convert(o.Author)
+        result
+
+    static member Convert(o: Models.Problems.ProblemSetPreviewModel) =
+        let result = ProblemSetPreview()
+        result.Id <- o.Id.Value
+        result.Title <- o.Title.Value
+        result.ProblemsCount <- o.ProblemsCount
+        result.DurationS <- uint32(o.Duration.Value.TotalSeconds)
+        result.Author <- Converter.Convert(o.Author)
+        result
+
+    static member Convert(o: Models.Heads.HeadModel) =
+        let result = Head()
+        result.Id <- o.Id.Value
+        result.Name <- o.Name.Value
+        result.Commit <- Converter.Convert(o.Commit)
+        Converter.Convert(o.Tags, result.Tags, fun x -> x.Value)
+        result
+
+    static member Convert(o: Models.Heads.TargetModel) =
+        let result = TargetModel()
+        result.Id <- o.Id.Value
+        result.Type <-
+            match o.Type with
+            | Models.Heads.ModelType.Problem -> TargetModel.Types.ModelType.Problem
+            | Models.Heads.ModelType.ProblemSet -> TargetModel.Types.ModelType.ProblemSet
+            | _ -> TargetModel.Types.ModelType.Unknown
+        result
+
+    static member Convert(o: Models.Heads.CommitModel) =
+        let result = Commit()
+        result.Id <- o.Id.Value
+        result.AuthorId <- o.AuthorId.Value
+        result.HeadId <- o.HeadId.Value
+        result.Target <- Converter.Convert(o.Target)
+        result.Timestamp <- Converter.Convert(o.Timestamp)
+        match o.ParentId with
+        | Some(id) -> result.ParentId <- id.Value
+        | None -> ()
+        result.Description <- o.Description.Value
+        result
+
+    static member Convert(o: Models.Reports.ReportModel) =
+        let result = Report()
+        result.Id <- o.Id.Value
+        result.ProblemSet <- Converter.Convert(o.ProblemSet)
+        result.StartedAt <- Converter.Convert(o.StartedAt)
+        result.FinishedAt <- Converter.Convert(o.FinishedAt)
+        result.Author <- Converter.Convert(o.Author)
+        result
+
+    static member Convert(o: Models.Reports.ProblemSetReportModel) =
+        let result = ProblemSetReport()
+        result.GeneratedProblemSetId <- o.Id.Value
+        result.Title <- o.Title.Value
+        Converter.Convert(o.Problems, result.Problems, Converter.Convert)
+        result
+
+    static member Convert(o: Models.Reports.ProblemReportModel) =
+        let result = ProblemReport()
+        result.GeneratedProblemId <- o.Id.Value
+        result.Title <- o.Title.Value
+        result.View <- Converter.Convert(o.View)
+        result.ExpectedAnswer <- o.ExpectedAnswer.Value
+        result.IsCorrect <- o.IsCorrect
+        match (o.Answer, o.Timespan) with
+        | (Some(ans), Some(ts)) ->
+            let answer = ProblemReport.Types.Answer()
+            answer.Value <- ans.Value
+            answer.Timestamp <- Converter.Convert(ts)
+            result.UserAnswer <- answer
+        | _ -> ()
+        result
+
+
+    static member Convert(o: Models.Reports.SubmissionModel) =
+        let result = Submission()
+        result.Id <- o.Id.Value
+        result.ProblemSet <- (Converter.Convert(o.ProblemSet))
+        result.StartedAt <- Converter.Convert(o.StartedAt)
+        result.Deadline <- Converter.Convert(o.Deadline)
+        result.Completed <- o.Completed
+        match o.ReportId with
+        | Some(reportId) -> result.ReportId <- reportId.Value
+        | None -> ()
+        result.Author <- Converter.Convert(o.Author)
+        Converter.Convert(o.Answers, result.Answers, Converter.Convert)
+        result
+
+    static member Convert(o: Models.Reports.ProblemAnswerModel) =
+        let result = ProblemAnswer()
+        result.Id <- o.GeneratedProblemId.Value
+        result.Answer <- o.Answer.Value
+        result.Timestamp <- Converter.Convert(o.Timestamp)
+        result
+
+    static member Convert(o: Models.Reports.SubmissionProblemSetModel) =
+        let result = SubmissionProblemSet()
+        result.Id <- o.Id.Value
+        result.Title <- o.Title.Value
+        Converter.Convert(o.Problems, result.Problems, Converter.Convert)
+        result
+
+    static member Convert(o: Models.Reports.SubmissionProblemModel) =
+        let result = SubmissionProblem()
+        result.Id <- o.Id.Value
+        result.Title <- o.Title.Value
+        result.View <- Converter.Convert(o.View)
+        result
+
+    static member Convert(o: Models.Problems.GeneratedViewModel) =
+        let result = GeneratedView()
+        result.Language <- Converter.Convert(o.Language.Language)
+        result.Content <- o.Content.Value
+        result
+
+    static member Convert(o: Models.Problems.ViewLanguage) =
+        match o with
+        | Models.Problems.ViewLanguage.Markdown -> View.Types.Language.Markdown
+        | Models.Problems.ViewLanguage.PlainText -> View.Types.Language.Plaintext
+        | Models.Problems.ViewLanguage.Tex -> View.Types.Language.Tex
+
+    static member Convert(o: Models.Permissions.UserModel) =
+        let result = User()
+        result.Id <- o.Id.Value
+        result.FirstName <- o.FirstName.Value
+        result.LastName <- o.LastName.Value
+        result.Username <- o.Username.Value
+        result
---
+++ Facade/ExaminationApi.fs
@@ -0,0 +1,406 @@
+﻿namespace Facade
+
+open Microsoft.AspNetCore.Authorization
+open GrpcExamination
+open Models.Permissions
+open Services.VersionControl
+open Services.Examination
+open Services.Permissions
+open Microsoft.Extensions.Logging
+open DatabaseTypes.Identificators
+open Grpc.Core
+open System.Security.Claims
+open Domain
+open Models.Problems
+open Models.Permissions
+open Models.Heads
+open Utils.ResultHelper
+
+[<Authorize>]
+type ExaminationApi(permissionsService: IPermissionsService,
+                    versionControlService: IVersionControlService,
+                    examinationService: IExaminationService,
+                    logger: ILogger<ExaminationApi>) =
+    inherit GrpcExamination.ExaminationService.ExaminationServiceBase()
+
+    member private this.GetUserId(context: ServerCallContext) = UserId(context.GetHttpContext().User.FindFirst(ClaimTypes.NameIdentifier).Value)
+
+    override this.GetSubmission(request, context) =
+        async {
+            let userId = this.GetUserId(context)
+            let submissionId = SubmissionId(request.SubmissionId)
+            match! permissionsService.CheckPermissions(ProtectedId.Submission(submissionId), userId, AccessModel.CanRead) with
+            | Ok() ->
+                match! examinationService.Get(submissionId) with
+                | Error(ex) ->
+                    logger.LogError(ex, "Cannot get submission")
+                    let error = GetSubmissionReply.Types.Error()
+                    error.Description <- ex.Message
+                    error.Status <- GetSubmissionReply.Types.Error.Types.Status.Unknown
+                    let reply = GetSubmissionReply()
+                    reply.Error <- error
+                    return reply
+                | Ok(model) ->
+                    let reply = GetSubmissionReply()
+                    reply.Submission <- Converter.Convert(model)
+                    return reply
+            | Error(ex) ->
+                let error = GetSubmissionReply.Types.Error()
+                error.Description <- ex.Message
+                error.Status <- GetSubmissionReply.Types.Error.Types.Status.NoAccess
+                let reply = GetSubmissionReply()
+                reply.Error <- error
+                return reply
+        }
+        |> Async.StartAsTask
+
+    override this.ApplyAnswer(request, context) =
+        async {
+            let userId = this.GetUserId(context)
+            let submissionId = SubmissionId(request.SubmissionId)
+            match! permissionsService.CheckPermissions(ProtectedId.Submission(submissionId), userId, AccessModel.CanWrite) with
+            | Ok() ->
+                let generatedProblemId = GeneratedProblemId(request.GeneratedProblemId)
+                let answer = ProblemAnswer(request.Answer)
+                match! examinationService.ApplyAnswer(generatedProblemId, answer, submissionId) with
+                | Error(ex) ->
+                    logger.LogError(ex, "Cannot apply answer")
+                    let error = ApplyAnswerReply.Types.Error()
+                    error.Description <- ex.Message
+                    error.Status <- ApplyAnswerReply.Types.Error.Types.Status.Unknown
+                    let reply = ApplyAnswerReply()
+                    reply.Error <- error
+                    return reply
+                | Ok(model) -> 
+                    let reply = ApplyAnswerReply()
+                    return reply
+            | Error(ex) ->
+                logger.LogError(ex, "Access denied")
+                let error = ApplyAnswerReply.Types.Error()
+                error.Description <- ex.Message
+                error.Status <- ApplyAnswerReply.Types.Error.Types.Status.NoAccess
+                let reply = ApplyAnswerReply()
+                reply.Error <- error
+                return reply
+        }
+        |> Async.StartAsTask
+
+    override this.CompleteSubmission(request, context) =
+        async {
+            let userId = this.GetUserId(context)
+            let submissionId = SubmissionId(request.SubmissionId)
+            match! permissionsService.CheckPermissions(ProtectedId.Submission(submissionId), userId, AccessModel.CanWrite) with
+            | Ok() ->
+                match! examinationService.Complete(submissionId, userId) with
+                | Error(ex) ->
+                    logger.LogError(ex, "Cannot complete")
+                    let error = CompleteSubmissionReply.Types.Error()
+                    error.Description <- ex.Message
+                    error.Status <- CompleteSubmissionReply.Types.Error.Types.Status.Unknown
+                    let reply = CompleteSubmissionReply()
+                    reply.Error <- error
+                    return reply
+                | Ok(model) -> 
+                    let reply = CompleteSubmissionReply()
+                    return reply
+            | Error(ex) ->
+                logger.LogError(ex, "Access denied")
+                let error = CompleteSubmissionReply.Types.Error()
+                error.Description <- ex.Message
+                error.Status <- CompleteSubmissionReply.Types.Error.Types.Status.NoAccess
+                let reply = CompleteSubmissionReply()
+                reply.Error <- error
+                return reply
+        }
+        |> Async.StartAsTask
+
+    override this.StartSubmission(request, context) =
+        async {
+            let userId = this.GetUserId(context)
+            let headId = HeadId(request.ProblemSetHeadId)
+            match! permissionsService.CheckPermissions(ProtectedId.Head(headId), userId, AccessModel.CanGenerate) with
+            | Ok() ->
+                match! versionControlService.Get(headId) with
+                | Error(ex) ->
+                    logger.LogError(ex, "Cannot get head for start")
+                    let error = StartSubmissionReply.Types.Error()
+                    error.Description <- ex.Message
+                    error.Status <- StartSubmissionReply.Types.Error.Types.Status.HeadNotFound
+                    let reply = StartSubmissionReply()
+                    reply.Error <- error
+                    return reply
+                | Ok(head) ->
+                    match head.Commit.Target.ConcreteId with
+                    | ConcreteId.ProblemSet(problemSetId) ->
+                        match! examinationService.CreateSubmission(problemSetId, userId) with
+                        | Error(ex) ->
+                            logger.LogError(ex, "Cannot create submission")
+                            let error = StartSubmissionReply.Types.Error()
+                            error.Description <- ex.Message
+                            error.Status <- StartSubmissionReply.Types.Error.Types.Status.CannotStartSubmission
+                            let reply = StartSubmissionReply()
+                            reply.Error <- error
+                            return reply
+                        | Ok(model) ->
+                            let reply = StartSubmissionReply()
+                            reply.SubmissionId <- model.Value
+                            return reply
+                    | _ ->
+                        logger.LogError("Invalid type")
+                        let error = StartSubmissionReply.Types.Error()
+                        error.Description <- "Invalid type of head"
+                        error.Status <- StartSubmissionReply.Types.Error.Types.Status.WrongHeadType
+                        let reply = StartSubmissionReply()
+                        reply.Error <- error
+                        return reply
+            | Error(ex) ->
+                logger.LogError(ex, "Access denied")
+                let error = StartSubmissionReply.Types.Error()
+                error.Description <- ex.Message
+                error.Status <- StartSubmissionReply.Types.Error.Types.Status.NoAccess
+                let reply = StartSubmissionReply()
+                reply.Error <- error
+                return reply
+        }
+        |> Async.StartAsTask
+
+    override this.GetReport(request, context) =
+        async {
+            let userId = this.GetUserId(context)
+            let reportId = ReportId(request.ReportId)
+            match! permissionsService.CheckPermissions(ProtectedId.Report(reportId), userId, AccessModel.CanRead) with
+            | Ok() ->
+                match! examinationService.Get(reportId) with
+                | Error(ex) ->
+                    logger.LogError(ex, "Cannot get report")
+                    let error = GetReportReply.Types.Error()
+                    error.Description <- ex.Message
+                    error.Status <- GetReportReply.Types.Error.Types.Status.Unknown
+                    let reply = GetReportReply()
+                    reply.Error <- error
+                    return reply
+                | Ok(model) ->
+                    let reply = GetReportReply()
+                    reply.Report <- Converter.Convert(model)
+                    return reply
+            | Error(ex) ->
+                logger.LogError(ex, "Access denied")
+                let error = GetReportReply.Types.Error()
+                error.Description <- ex.Message
+                error.Status <- GetReportReply.Types.Error.Types.Status.NoAccess
+                let reply = GetReportReply()
+                reply.Error <- error
+                return reply
+        }
+        |> Async.StartAsTask
+
+    override this.GetSubmissions(request, context) =
+        async {
+            let userId = this.GetUserId(context)
+            match! examinationService.GetSubmissions(userId) with
+            | Error(ex) ->
+                logger.LogError(ex, "Cannot get submissions")
+                let error = GetSubmissionsReply.Types.Error()
+                error.Description <- ex.Message
+                error.Status <- GetSubmissionsReply.Types.Error.Types.Status.Unknown
+                let reply = GetSubmissionsReply()
+                reply.Error <- error
+                return reply
+            | Ok(models) ->
+                let submissions = GetSubmissionsReply.Types.Submissions()
+                Converter.Convert(models, submissions.SubmissionIds, fun x -> x.Value)
+                let reply = GetSubmissionsReply()
+                reply.Submissions <- submissions
+                return reply
+        }
+        |> Async.StartAsTask
+
+    override this.GetReports(request, context) =
+        async {
+            let userId = this.GetUserId(context)
+            let pattern =
+                match request.Pattern with
+                | null -> None
+                | str -> Some(str)
+            let authorId =
+                match request.UserId with
+                | null -> None
+                | id -> Some(UserId(id))
+
+            let date =
+                match request.DateInterval with
+                | null -> None
+                | d -> Some(BackConverter.Convert(d))
+
+            match! examinationService.Search(pattern, userId, authorId, date, request.Offset, request.Limit) with
+            | Error(ex) ->
+                logger.LogError(ex, "Cannot get reports by author")
+                let error = GetReportsReply.Types.Error()
+                error.Description <- ex.Message
+                error.Status <- GetReportsReply.Types.Error.Types.Status.Unknown
+                let reply = GetReportsReply()
+                reply.Error <- error
+                return reply
+            | Ok(models) ->
+                let reportsList = GetReportsReply.Types.ReportsList()
+                Converter.Convert(models, reportsList.Reports, Converter.Convert)
+                let reply = GetReportsReply()
+                reply.Reports <- reportsList
+                return reply
+        }
+        |> Async.StartAsTask
+
+    override this.ShareReport(request, context) =
+        async {
+            let userId = this.GetUserId(context)
+            let reportId = ReportId(request.ReportId)
+            match! permissionsService.CheckPermissions(ProtectedId.Report(reportId), userId, AccessModel.CanAdministrate) with
+            | Ok() ->
+                let! result =
+                    request.UserIds
+                    |> Seq.map(fun id -> permissionsService.Share(ProtectedId.Report(reportId), UserId(id)))
+                    |> Seq.append(
+                        request.GroupIds
+                        |> Seq.map(fun id -> permissionsService.Share(ProtectedId.Report(reportId), GroupId(id)))
+                    )
+                    |> ResultOfAsyncSeq
+                match result with
+                | Error(ex) ->
+                    logger.LogError(ex, "Cannot share report")
+                    let error = ShareReportReply.Types.Error()
+                    error.Description <- ex.Message
+                    error.Status <- ShareReportReply.Types.Error.Types.Status.Unknown
+                    let reply = ShareReportReply()
+                    reply.Error <- error
+                    return reply
+                | Ok(model) ->
+                    let reply = ShareReportReply()
+                    return reply
+            | Error(ex) ->
+                logger.LogError(ex, "Access denied")
+                let error = ShareReportReply.Types.Error()
+                error.Description <- ex.Message
+                error.Status <- ShareReportReply.Types.Error.Types.Status.NoAccess
+                let reply = ShareReportReply()
+                reply.Error <- error
+                return reply
+        }
+        |> Async.StartAsTask
+
+    override this.GetProblemSets(request, context) =
+        async {
+            let userId = this.GetUserId(context)
+            let pattern =
+                match request.Pattern with
+                | null -> None
+                | p -> Some(p)
+            let tags = List.ofSeq request.Tags |> List.map(fun x -> TagModel(x))
+            let authorId =
+                match request.AuthorId with
+                | null -> None
+                | id -> Some(UserId(id))
+            let problemsCount =
+                match request.ProblemsCount with
+                | null -> None
+                | interval -> Some(BackConverter.Convert(interval))
+            let duration =
+                match request.DurationS with
+                | null -> None
+                | interval -> Some(BackConverter.Convert(interval))
+            match! examinationService.GetProblemSets(request.IsPublic,
+                                                     userId,
+                                                     pattern,
+                                                     tags,
+                                                     authorId,
+                                                     problemsCount,
+                                                     duration,
+                                                     request.Offset,
+                                                     request.Limit) with
+            | Error(ex) ->
+                logger.LogError(ex, "Cannot get problem sets")
+                let error = GetProblemSetsReply.Types.Error()
+                error.Description <- ex.Message
+                error.Status <- GetProblemSetsReply.Types.Error.Types.Status.Unknown
+                let reply = GetProblemSetsReply()
+                reply.Error <- error
+                return reply
+            | Ok(models) ->
+                let headList = GetProblemSetsReply.Types.HeadList()
+                Converter.Convert(models, headList.Heads, Converter.Convert)
+                let reply = GetProblemSetsReply()
+                reply.Heads <- headList
+                return reply
+        }
+        |> Async.StartAsTask
+
+    override this.GetProblemSetPreview(request, context) =
+        async {
+            let userId = this.GetUserId(context)
+            let commitId = CommitId(request.CommitId)
+
+            match! versionControlService.Get(commitId) with
+            | Error(ex) ->
+                logger.LogError(ex, "Cannot get commit for problem set preview")
+                let error = GetProblemSetPreviewReply.Types.Error()
+                error.Description <- ex.Message
+                error.Status <- GetProblemSetPreviewReply.Types.Error.Types.Status.NotFound
+                let reply = GetProblemSetPreviewReply()
+                reply.Error <- error
+                return reply
+            | Ok(commit) ->
+                match! permissionsService.CheckPermissions(ProtectedId.Head(commit.HeadId), userId, AccessModel.CanGenerate) with
+                | Error(ex) ->
+                    logger.LogError(ex, "Access denied")
+                    let error = GetProblemSetPreviewReply.Types.Error()
+                    error.Description <- ex.Message
+                    error.Status <- GetProblemSetPreviewReply.Types.Error.Types.Status.NoAccess
+                    let reply = GetProblemSetPreviewReply()
+                    reply.Error <- error
+                    return reply
+                | Ok(_) ->
+                    match! examinationService.GetProblemSetPreview(commitId) with
+                    | Error(ex) ->
+                        logger.LogError(ex, "Cannot get problem set preview")
+                        let error = GetProblemSetPreviewReply.Types.Error()
+                        error.Description <- ex.Message
+                        error.Status <- GetProblemSetPreviewReply.Types.Error.Types.Status.Unknown
+                        let reply = GetProblemSetPreviewReply()
+                        reply.Error <- error
+                        return reply
+                    | Ok(model) ->
+                        let reply = GetProblemSetPreviewReply()
+                        reply.Preview <- Converter.Convert(model)
+                        return reply
+        }
+        |> Async.StartAsTask
+
+    override this.GetSubmissionPreview(request, context) =
+        async {
+            let userId = this.GetUserId(context)
+            let submissionId = SubmissionId(request.SubmissionId)
+
+            match! permissionsService.CheckPermissions(ProtectedId.Submission(submissionId), userId, AccessModel.CanRead) with
+            | Error(ex) ->
+                logger.LogError(ex, "Access denied")
+                let error = GetSubmissionPreviewReply.Types.Error()
+                error.Description <- ex.Message
+                error.Status <- GetSubmissionPreviewReply.Types.Error.Types.Status.NoAccess
+                let reply = GetSubmissionPreviewReply()
+                reply.Error <- error
+                return reply
+            | Ok(_) ->
+                match! examinationService.GetPreview(submissionId) with
+                | Error(ex) ->
+                    logger.LogError(ex, "Cannot get submission preview")
+                    let error = GetSubmissionPreviewReply.Types.Error()
+                    error.Description <- ex.Message
+                    error.Status <- GetSubmissionPreviewReply.Types.Error.Types.Status.Unknown
+                    let reply = GetSubmissionPreviewReply()
+                    reply.Error <- error
+                    return reply
+                | Ok(model) ->
+                    let reply = GetSubmissionPreviewReply()
+                    reply.Preview <- Converter.Convert(model)
+                    return reply
+        }
+        |> Async.StartAsTask
